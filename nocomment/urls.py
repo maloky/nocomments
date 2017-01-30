fbe5abbc956444bd3622cf223f35a45882c55eb9
Jorge Costa <joky@maloky.com>
2017-01-30 16:04:45 +0000

    prefix api url

--- nocomment/urls.py
+++ nocomment/urls.py
@@ -29,7 +29,6 @@ router.register(r'comments', CommentsViewSet)
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^api/', include(router.urls, namespace='api_urls')),
 ]
-
-urlpatterns += router.urls
