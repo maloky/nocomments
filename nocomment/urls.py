d1efd77998692cac6b00714c243440da5b1365c3
Jorge Costa <joky@maloky.com>
2017-01-31 13:51:50 +0000

    move html to template folder

--- nocomment/urls.py
+++ nocomment/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-
+from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 
 from rest_framework import routers
-
 
 from comments.api import CommentsViewSet
 
@@ -27,8 +28,9 @@ router = routers.SimpleRouter()
 router.register(r'comments', CommentsViewSet)
 
 urlpatterns = [
+    url(r'^$', TemplateView.as_view(template_name='blog.html')),
     url(r'^admin/', admin.site.urls),
 
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^api/', include(router.urls, namespace='api_urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
