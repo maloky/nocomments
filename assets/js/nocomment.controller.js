6340ee0ae0b66d23f47e3bf7c79c3e1464568fa7
Jorge Costa <joky@maloky.com>
2017-02-01 18:33:49 +0000

    move field reset logic to self function

--- assets/js/nocomment.controller.js
+++ assets/js/nocomment.controller.js
@@ -7,15 +7,6 @@ function nocommentController(nocommentService) {
     ctrl.link = 'http://127.0.0.1:8000';
     ctrl.list = [];
 
-    function getAllComments() {
-      nocommentService
-        .getComments()
-        .then( function (response ){
-            ctrl.list = response;
-        })
-    };
-
-
     ctrl.addComment = function() {
       if (!ctrl.comment) {
             return;
@@ -29,11 +20,22 @@ function nocommentController(nocommentService) {
             link: ctrl.link})
         .then( function (response ){
           ctrl.list.push(response);
-          ctrl.name = '';
-          ctrl.email = '';
-          ctrl.comment = '';
-
+          clearForm();
         });
+    };
+
+    function getAllComments() {
+      nocommentService
+        .getComments()
+        .then( function (response ){
+            ctrl.list = response;
+        })
+    };
+
+    function clearForm() {
+      ctrl.name = '';
+      ctrl.email = '';
+      ctrl.comment = '';
     };
 
     getAllComments();
