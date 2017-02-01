d310865ca5f2762244891449eec8a19b1f258af8
Jorge Costa <joky@maloky.com>
2017-02-01 22:10:55 +0000

    add custom action to admin comments list view

--- comments/admin.py
+++ comments/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 from .models import Comment
 
-# Register your models here.
-admin.site.register(Comment)
+
+def make_visible(modeladmin, request, queryset):
+    queryset.update(visible=True)
+make_visible.short_description = "Mark selected comment as visible"
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('email', 'comment', 'visible')
+    list_filter = ('visible',)
+    actions = [make_visible]
+
+admin.site.register(Comment, CommentAdmin)
