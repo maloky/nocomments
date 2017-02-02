32a00ad9cef61d2eb20c5e10bbd3ca1d6d4fbcd3
Jorge Costa <joky@maloky.com>
2017-02-02 00:15:46 +0000

    enable debug for testing porpouses

--- nocomment/settings.py
+++ nocomment/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-)r-e-cai_w8^^9==)p%dcvy-&5d@tomlor*47d&wvlt9kb&3+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['nocomments.herokuapp.com']
 
