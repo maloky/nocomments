6e3791502d6bdc4553d86c92c784f7d6c89399c3
Jorge Costa <joky@maloky.com>
2017-02-02 10:29:51 +0000

    enable csrf token for API requests

--- assets/js/nocomment.app.js
+++ assets/js/nocomment.app.js
@@ -1,5 +1,8 @@
 angular
-    .module('nocommentApp', []).config(function($interpolateProvider) {
-        $interpolateProvider.startSymbol('{$');
-        $interpolateProvider.endSymbol('$}');
+    .module('nocommentApp', []).config(
+        function($interpolateProvider, $httpProvider) {
+            $interpolateProvider.startSymbol('{$');
+            $interpolateProvider.endSymbol('$}');
+            $httpProvider.defaults.xsrfCookieName = 'csrftoken';
+            $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
     });
